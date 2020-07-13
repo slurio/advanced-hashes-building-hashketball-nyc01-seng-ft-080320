8368e041e09e9c173fb4437a961ec4720c59170d
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-13 00:46:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,25 +58,25 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [{
-        :player_name => "",
-        :number => 0,
-        :shoe => 0,
-        :points => 0,
-        :rebounds => 0,
-        :assists => 0,
-        :steals => 0,
-        :blocks => 0,
-        :slam_dunks => 0},
+        :player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2},
       {
-        :player_name => "",
+        :player_name => "Bismack Biyombo",
         :number => 0,
-        :shoe => 0,
-        :points => 0,
-        :rebounds => 0,
-        :assists => 0,
-        :steals => 0,
-        :blocks => 0,
-        :slam_dunks => 0},]
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5},]
     }
   }
 end
