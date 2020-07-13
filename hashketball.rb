25121ab391addc9125aa2de18090a21980309e47
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-13 00:44:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,17 @@ def game_hash
         :points => 0,
         :rebounds => 0,
         :assists => 0,
-      :steals => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0},
+      {
+        :player_name => "",
+        :number => 0,
+        :shoe => 0,
+        :points => 0,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
         :blocks => 0,
         :slam_dunks => 0},]
     }
