8221aacaea5aec86f7e65f281ca4919b6bad8997
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-13 00:32:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
         :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebonds => 12,
+        :rebounds => 12,
         :assists => 12,
         :steals => 3,
         :blocks => 1,
