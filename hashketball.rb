a6c958feceb99d617702646562fbcefa4815fc10
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-13 00:31:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,15 +4,15 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [{
-        :player_name => "",
-        :number => "",
-        :shoe => "",
-        :points => "",
-        :rebonds => "",
-        :assists => "",
-        :steals => "",
-        :blocks => "",
-        :slam_dunks => ""
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebonds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       },]
     },
     :away => {
