940cb73f4a47c90278bbdad48a9319c5a5a72f71
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-13 00:27:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,17 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => []
+      :players => [
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebonds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
